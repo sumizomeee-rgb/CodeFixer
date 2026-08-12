05b22046cfc34ee2402b892bf3ba760a0819b33c
sumizomeee <sumizomeee@gmail.com>
2026-08-12 09:50:30 +0800

    feat: export artifact protocols

---
+++ backend/src/codefixer/protocols/__init__.py
@@ -0,0 +1,17 @@
+from .artifacts import (
+    ArtifactProtocolError,
+    ArtifactStore,
+    SchemaRegistry,
+    StageEntry,
+    StoredArtifact,
+    render_stage_entry,
+)
+
+__all__ = [
+    "ArtifactProtocolError",
+    "ArtifactStore",
+    "SchemaRegistry",
+    "StageEntry",
+    "StoredArtifact",
+    "render_stage_entry",
+]
