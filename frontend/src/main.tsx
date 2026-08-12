a66295171259320965d8c2594a9088d3be269595
sumizomeee <sumizomeee@gmail.com>
2026-08-12 10:10:53 +0800

    feat: load unified product visual layer

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
-import './phase1.css'
-import './phase2.css'
+import './product.css'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
